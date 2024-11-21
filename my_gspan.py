a02da48b467fbb9c9d7eadecaa298a8af49f3bd1
YukiCheZ <yukichezzzz@gmail.com>
2024-11-21 12:31:49 +0800

    generate result (but acc low)

--- my_gspan.py
+++ my_gspan.py
@@ -1,5 +1,6 @@
 import collections
 import itertools
+import json
 from my_graph import Graph
 from my_graph import VACANT_GRAPH_ID
 from my_graph import AUTO_EDGE_ID
@@ -16,6 +17,12 @@ card_id_offset = 800000
 # 辅助函数
 def map_name(value):
     return name_mapping.get(value, -1)
+
+def decode_name(value):
+    for k, v in name_mapping.items():
+        if v == value:
+            return k
+    return "Unknown"
 
 def extract_last_char_as_int(value):
     try:
@@ -44,6 +51,7 @@ def construct_graph(data_dir):
                 vid = int(id_str)
                 name_int = map_name(name)
                 graph.add_vertex(vid, {'type' : v_type_mapping['account'], 'name': name_int})
+                # graph.add_vertex(vid, {'name': name_int})
     except Exception as e:
         print(f"Error reading account file: {e}")
 
@@ -55,6 +63,7 @@ def construct_graph(data_dir):
                 vid = map_card_id(id_str)
                 name_int = map_name(name)
                 graph.add_vertex(vid, {'type' :  v_type_mapping['card'], 'name': name_int})
+                # graph.add_vertex(vid, {'name': name_int})
     except Exception as e:
         print(f"Error reading card file: {e}")
 
@@ -182,7 +191,7 @@ def pruning_3edge_frequent_subgraph(sub_1e_g, sub_2e_patterns, min_support):
     return frequent_sub_3e_patterns_mode1, frequent_sub_3e_patterns_mode2
 
 
-def generate_result_json(frequent_sub_3e_patterns_mode1, frequent_sub_3e_patterns_mode2)
+def generate_result_json(frequent_sub_3e_patterns_mode1, frequent_sub_3e_patterns_mode2):
     result = []
 
     for key, support in frequent_sub_3e_patterns_mode1.items():
@@ -200,15 +209,109 @@ def generate_result_json(frequent_sub_3e_patterns_mode1, frequent_sub_3e_pattern
         e2_att = dict(edge2_attrs)
         e3_att = dict(edge3_attrs)
         nodes.append({
-            'id': v1_att['vid'],
-            'label': v1_att['name']
+            "node_id": "0",
+            "name": decode_name(v1_att['name'])
+        })
+        nodes.append({
+            "node_id": "1",
+            "name": decode_name(v2_att['name'])
+        })
+        nodes.append({
+            "node_id": "2",
+            "name": decode_name(v3_att['name'])
+        })
+        nodes.append({
+            "node_id": "3",
+            "name": decode_name(v4_att['name'])
+        })
+        edges.append({
+            "source_node_id": "0",
+            "target_node_id": "1",
+            "amt": str(e1_att['amt']),
+            "strategy_name": str(e1_att['strategy_name']),
+            "buscode": str(e1_att['buscode'])
+        })
+        edges.append({
+            "source_node_id": "1",
+            "target_node_id": "2",
+            "amt": str(e2_att['amt']),
+            "strategy_name": str(e2_att['strategy_name']),
+            "buscode": str(e2_att['buscode'])
+        })
+        edges.append({
+            "source_node_id": "2",
+            "target_node_id": "3",
+            "amt": str(e3_att['amt']),
+            "strategy_name": str(e3_att['strategy_name']),
+            "buscode": str(e3_att['buscode'])
+        })
+        result.append({
+            "frequency": support,
+            "nodes": nodes,
+            "edges": edges
         })
 
 
+    for key, support in frequent_sub_3e_patterns_mode2.items():
+        nodes = []
+        edges = []
+        key1, key2, key3 = key
+        edge1_attrs, vertex1_attrs, vertex2_attrs = key1
+        edge2_attrs, _, vertex3_attrs = key2
+        edge3_attrs, _, _ = key3
+        v1_att = dict(vertex1_attrs)
+        v2_att = dict(vertex2_attrs)
+        v3_att = dict(vertex3_attrs)
+        e1_att = dict(edge1_attrs)
+        e2_att = dict(edge2_attrs)
+        e3_att = dict(edge3_attrs)
+        nodes.append({
+            "node_id": "0",
+            "name": decode_name(v1_att['name'])
+        })
+        nodes.append({
+            "node_id": "1",
+            "name": decode_name(v2_att['name'])
+        })
+        nodes.append({
+            "node_id": "2",
+            "name": decode_name(v3_att['name'])
+        })
+        edges.append({
+            "source_node_id": "0",
+            "target_node_id": "1",
+            "amt": str(e1_att['amt']),
+            "strategy_name": str(e1_att['strategy_name']),
+            "buscode": str(e1_att['buscode'])
+        })
+        edges.append({
+            "source_node_id": "1",
+            "target_node_id": "2",
+            "amt": str(e2_att['amt']),
+            "strategy_name": str(e2_att['strategy_name']),
+            "buscode": str(e2_att['buscode'])
+        })
+        edges.append({
+            "source_node_id": "2",
+            "target_node_id": "0",
+            "amt": str(e3_att['amt']),
+            "strategy_name": str(e3_att['strategy_name']),
+            "buscode": str(e3_att['buscode'])
+        })
+        result.append({
+            "frequency": support,
+            "nodes": nodes,
+            "edges": edges
+        })
 
+    with open("result.json", "w") as file:
+        json.dump(result, file, indent=2)
+
+    print("Result JSON generation completed.")
 
 sub_1e_g = pruning_1edge_frequent_subgraph(10000)
 fre_sub_2e_patterns = pruning_2edge_frequent_subgraph(sub_1e_g, 10000)
 fre_sub_3e_patterns_mode1, fre_sub_3e_patterns_mode2 = pruning_3edge_frequent_subgraph(sub_1e_g, fre_sub_2e_patterns, 10000)
-
-print(f"Number of frequent 3-edge subgraphs: {len(fre_sub_3e_patterns_mode1) + len(fre_sub_3e_patterns_mode2)}")
+print(f"Number of frequent 3-edge subgraphs mode1: {len(fre_sub_3e_patterns_mode1)}")
+print(f"Number of frequent 3-edge subgraphs mode2: {len(fre_sub_3e_patterns_mode2)}")
+generate_result_json(fre_sub_3e_patterns_mode1, fre_sub_3e_patterns_mode2)
